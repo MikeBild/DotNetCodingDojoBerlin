07197d07b07cc2b5175e6ef192527f5ccf556b19
Mike Bild <mike@mikebild.com>
2010-04-01 07:10:35 +0200

    Verzeichnisstruktur angepasst

---
+++ FizzBuzzKata/source/FizzBuzz/AnwendungsSchicht.FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AnwendungsSchicht.FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        //private string _ausgabe;
+        public string MacheFizzBuzzVon1Bis100()
+        {
+            string ausgabe = "";
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0)
+                    ausgabe += AddFizz();
+                if (i % 5 == 0)
+                    ausgabe += AddBuzz();
+                if (i % 3 != 0 && i % 5 != 0)
+                    ausgabe += AddZahl(i);
+                
+                ausgabe += AddLeerzeichen();
+            }
+            return ausgabe;
+        }
+
+        private String AddZahl(int i)
+        {
+            return i.ToString();
+        }
+
+        private string AddLeerzeichen()
+        {
+            return " ";
+        }
+
+        private string AddBuzz()
+        {
+            return "Buzz";
+        }
+
+        private string AddFizz()
+        {
+            return "Fizz";
+        }
+    }
+}
---
+++ FizzBuzzKata/source/FizzBuzz/FizzBuzzPortal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using AnwendungsSchicht.FizzBuzz;
+
+namespace FizzBuzzPortal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fb = new FizzBuzzer();
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                var t1 = new System.Threading.Thread(() =>
+                {
+                
+                    fb.MacheFizzBuzzVon1Bis100();
+                    Console.WriteLine(fb.MacheFizzBuzzVon1Bis100());
+
+                });
+                t1.Start();
+            }
+
+            
+            //Console.WriteLine(new FizzBuzzer().MacheFizzBuzzVon1Bis100());
+            Console.ReadLine();
+        }
+    }
+}
