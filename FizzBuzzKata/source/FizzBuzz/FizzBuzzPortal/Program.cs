552448703800448f0f948b0275634a32738a4c8c
Mike Bild <mike@mikebild.com>
2010-04-01 07:16:52 +0200

    Das ganze noch mal ohne Multithreading

--- FizzBuzzKata/source/FizzBuzz/FizzBuzzPortal/Program.cs
+++ FizzBuzzKata/source/FizzBuzz/FizzBuzzPortal/Program.cs
@@ -8,24 +8,7 @@ namespace FizzBuzzPortal
     {
         static void Main(string[] args)
         {
-
-            var fb = new FizzBuzzer();
-
-            for (int i = 0; i < 10; i++)
-            {
-
-                var t1 = new System.Threading.Thread(() =>
-                {
-                
-                    fb.MacheFizzBuzzVon1Bis100();
-                    Console.WriteLine(fb.MacheFizzBuzzVon1Bis100());
-
-                });
-                t1.Start();
-            }
-
-            
-            //Console.WriteLine(new FizzBuzzer().MacheFizzBuzzVon1Bis100());
+            Console.WriteLine(new FizzBuzzer().MacheFizzBuzzVon1Bis100());
             Console.ReadLine();
         }
     }
